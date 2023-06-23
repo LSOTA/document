cf21e5346a66f9fa666cd211a427c21033c88c83
LSota19git config --global user.name LSota19git config --global user.name LSota19 <leonard.sota@rainbowpartners>
2023-06-23 15:42:08 +0200

    application

---
+++ 7-zip/7-zip.ps1
@@ -0,0 +1,1 @@
+choco download 7zip.install --internalize --version='23.1.0' --source=https://community.chocolatey.org/api/v2/
---
+++ AdobeReader/Adobereader.ps1
@@ -0,0 +1,1 @@
+choco download adobereader --internalize --version='2023.3.20201.1' --source=https://community.chocolatey.org/api/v2/
